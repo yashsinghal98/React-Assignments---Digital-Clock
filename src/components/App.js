9c105317acf09d50e304708a6d2747492ebfcb2b
yashsinghal98 <68867021+yashsinghal98@users.noreply.github.com>
2020-10-30 16:15:32 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -19,11 +19,18 @@ class App extends Component {
     return (
       <div className="Clock">
         <h3 id="time">
-          {this.state.dat.getHours()>12?this.state.dat.getHours()%12:this.state.dat.getHours()}:
-          {this.state.dat.getMinutes()}
+          {this.state.dat.getHours() % 12 < 10
+            ? `0${this.state.dat.getHours() % 12}`
+            : `${this.state.dat.getHours()}`}
           :
-          {this.state.dat.getSeconds()}{" "}
-          {this.state.dat.getHours < 12 ? "AM" : "PM"}
+          {this.state.dat.getMinutes() < 10
+            ? `0${this.state.dat.getMinutes()}`
+            : `${this.state.dat.getMinutes()}`}
+          :
+          {this.state.dat.getSeconds() < 10
+            ? `0${this.state.dat.getSeconds() % 12}`
+            : `${this.state.dat.getSeconds()}`}{" "}
+          {this.state.dat.getHours < 12  ? "AM" : "PM"}
         </h3>
       </div>
     );
