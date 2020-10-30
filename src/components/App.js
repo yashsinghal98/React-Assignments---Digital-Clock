9139208ce7750c704ce799ca335fa2d167b75086
yashsinghal98 <68867021+yashsinghal98@users.noreply.github.com>
2020-10-30 15:26:41 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,37 @@
-import React, {Component} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor() {
-      super();
-      this.state = { dat: new Date() };
-      this.loop = () => {
-        setInterval(() => {
-          this.setState({ dat: new Date() });
-        }, 1000);
-      };
+  constructor() {
+    super();
+    this.state = { dat: new Date() };
+    this.loop = () => {
+      setInterval(() => {
+        this.setState({ dat: new Date() });
+      }, 1000);
     };
-    componentDidMount() {
-      this.loop();
-    }
-    componentWillUnmount() {
-      clearInterval(this.loop());
-    }
-    render() {
-      return <div className="Clock"><div className="time">{this.state.dat.getHours()%12}:{this.state.dat.getMinutes()}:{this.state.dat.getSeconds()} {this.state.dat.getHours%12===0?"AM":"PM"}</div></div>;
-    }
   }
-  
-  export default App;
-  
+  componentDidMount() {
+    this.loop();
+  }
+  componentWillUnmount() {
+    clearInterval(this.loop());
+  }
+  render() {
+    return (
+      <div className="Clock">
+        <div className="time">
+        {this.state.dat.getHours() % 12<10?`0${this.state.dat.getHours()%12}`:`${this.state.dat.getHours()}`}
+        :
+        {this.state.dat.getMinutes()<10?`0${this.state.dat.getMinutes()}`:`${this.state.dat.getMinutes()}`}
+        :
+        {this.state.dat.getSeconds()<10?`0${this.state.dat.getSeconds()%12}`:`${this.state.dat.getSeconds()}`}
+        {" "}
+        {this.state.dat.getHours % 12 === 0 ? "AM" : "PM"}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
