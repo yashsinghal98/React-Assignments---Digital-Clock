5aecaf53c91b38b304089056310f81622e727ec1
yashsinghal98 <68867021+yashsinghal98@users.noreply.github.com>
2020-10-30 15:09:07 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
-import React, {Component, useState} from "react";
+import React, {Component} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
+    constructor() {
+      super();
+      this.state = { dat: new Date() };
+      this.loop=null;
+    };
+    componentDidMount() {
+      let val=new Date();
+      this.loop=setInterval(()=>this.setState({dat:val}),1000);
     }
-}
-
-
-export default App;
+    componentWillUnmount() {
+      clearInterval(this.loop);
+    }
+    render() {
+      return <div className="Clock"><div className="time">{this.state.dat.getHours()%12}:{this.state.dat.getMinutes()}:{this.state.dat.getSeconds()} {this.state.dat.getHours%12===0?"AM":"PM"}</div></div>;
+    }
+  }
+  
+  export default App;
+  
